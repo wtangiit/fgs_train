bfe6e7cb2da09ae2b96701e30e6c2c2f6146e777
Will Trimble <trimble@anl.gov>
2012-11-05 16:26:04 -0600

    refined environment setup

--- src/fgs_train_environment.sh
+++ src/fgs_train_environment.sh
@@ -2,11 +2,13 @@
 # Prepare environment for FGS retraining 
 
 sudo apt-get update
-sudo apt-get install git
+sudo apt-get install git -y
 sudo apt-get install python-biopython -y
 sudo chown ubuntu /mnt
 
 genomedir=/mnt/genomes
+linksdir=/mnt/genomes-flat
+workdir=/mnt/work
 
 mkdir build
 cd build
@@ -14,23 +16,28 @@ git clone https://github.com/wtangiit/fgs_train.git
 git clone https://github.com/wltrimbl/FGS.git
 
 echo export PATH=\$PATH:$HOME/build/FGS:$HOME/build/fgs_train/src >> ~/.profile
+source ~/.profile
 
 mkdir $genomedir
 curl ftp://ftp.ncbi.nih.gov/genomes/Bacteria/all.fna.tar.gz > $genomedir/all.fna.tar.gz
 curl ftp://ftp.ncbi.nih.gov/genomes/Bacteria/all.ptt.tar.gz > $genomedir/all.ptt.tar.gz
+
+cd $genomedir 
 tar xvf all.fna.tar.gz 
 tar xvf all.ptt.tar.gz
 
-ln -s genomes/Acinetobacter_baumannii_AB307_0294_uid59271/* /mnt
-cd /mnt
+mkdir $linksdir
+ln -s $genomedir/*/* $linksdir
 
-gen_train_input.py -i NC_011595.fna -p NC_011595.ptt
+mkdir $workdir
+cd $workdir
+gen_train_input.py -i $linksdir/NC_011595.fna -p $linksdir/NC_011595.ptt
 # generates 
 #    train_fwd_NC_011595.fna.csv
 #    train_noncoding_NC_011595.fna.csv
 
-validate_train_input.py -i train_fwd_NC_011595.fna.csv | tee codonstats.txt
+validate_train_input.py -i train_fwd_NC_011595.csv | tee codonstats.txt
 
-fgs_train.py  -i train_fwd_NC_011595.fna.csv -n train_noncoding_NC_011595.fna.csv
+fgs_train.py  -i train_fwd_NC_011595.csv -n train_noncoding_NC_011595.csv
 # generates gene, rgene, start, start1, stop, stop1, gene.ct, rgene.ct
 
